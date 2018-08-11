1aa0f6a556d361a4d87b4338c9230347c689e768
root <ibuioli@gmail.com>
2018-08-10 22:23:24 -0300

    Better function on Chart

--- static/src/js/website.js
+++ static/src/js/website.js
@@ -9,74 +9,96 @@ odoo.define('stock_d3.index', function (require) {
     var Widget = require('web.Widget');
     var website = require('website.website');
     var treeData = [];
-    /*var treeData = [
-      {
-        "name": "0000000000017",
-        "parent": "null",
-        "children": [
-          {
-            "name": "999-777-01",
-            "parent": "0000000000017",
-            "children": [
-              {
-                "name": "230000111111",
-                "parent": "999-777-01"
-              },
-              {
-                "name": "1234-5555",
-                "parent": "999-777-01"
-              }
-            ]
-          },
-          {
-            "name": "1000222",
-            "parent": "0000000000017"
-          }
-        ]
-      }
-    ];//Demo Tree Data*/
 
     var id = getQueryVariable("id");
+    var children = [];
 
     var Lots = new Model('stock.production.lot');
-    Lots.query(['id','name']).filter([['name', '=', id]]).all().then(function(data) {
+    Lots.query(['id','name', 'product_id']).filter([['name', '=', id]]).all().then(function(data) {
+      var product = limitChars(data[0].product_id[1]);
       treeData = [
         {
           "name": data[0].name,
+          "product": product,
           "parent": "null",
           "children": []
         }
       ];
 
       var MoveLots = new Model('stock.move.lots');
-      MoveLots.query(['lot_id']).filter([['destination_lot_id', '=', data[0].name]]).all().then(function(childdata) {
+      MoveLots.query(['lot_id', 'product_id']).filter([['destination_lot_id', '=', data[0].name]]).all().then(function(childdata) {
         for (var i = 0; i < childdata.length; i++) {
-          treeData[0].children.push({
+          var product = limitChars(childdata[i].product_id[1]);
+          children.push({
             "name": childdata[i].lot_id[1],
-            "parent": data[0].name
+            "product": product,
+            "parent": data[0].name,
+            "children": []
           });
+          recurChild(childdata[i].lot_id[1], children[i]);
         }
-        drawGraph();
+
+        setTimeout(function(){
+          children.length = childdata.length;
+          treeData[0].children = children;
+          drawGraph();
+        }, 1000);
       });
     });
+
+    function recurChild(parent, c){
+      var ChildLots = new Model('stock.move.lots');
+      ChildLots.query(['lot_id', 'product_id']).filter([['destination_lot_id', '=', parent]]).all().then(function(indata) {
+        for (var i = 0; i < indata.length; i++) {
+          var product = limitChars(indata[i].product_id[1]);
+          c.children.push({
+            "name": indata[i].lot_id[1],
+            "product": product,
+            "parent": parent,
+            "children": []
+          });
+          children.push(c);
+          recurChild(indata[i].lot_id[1], c.children[i]);
+        }
+      });
+    }
+
+    function limitChars(str){
+      if(str){
+        if(str.length > 17){
+          return "("+str.replace(/\[(.+?)\] /g, "").substring(0, 12) + "...)";
+        }else{
+          return "("+str.replace(/\[(.+?)\] /g, "")+")";
+        }
+      }else{
+        return "()";
+      }
+    }
 
     function drawGraph(){
       setTimeout(function(){
         // ************** Generate the tree diagram	 *****************
-
-        var margin = {top: 10, right: 120, bottom: 10, left: 120},
-        width = 960 - margin.right - margin.left,
-        height = 500 - margin.top - margin.bottom;
+        var margin = {top: 50, right: 120, bottom: 10, left: (window.innerWidth-250) / 2},
+        width = (window.innerWidth-250) - margin.right - margin.left,
+        height = 600 - margin.top - margin.bottom;
 
         var i = 0,
           duration = 750,
           root;
 
+        var nodeWidth = 110;
+        var nodeHeight = 75;
+        var horizontalSeparationBetweenNodes = 16;
+        var verticalSeparationBetweenNodes = 128;
+
         var tree = d3.layout.tree()
-          .size([height, width]);
+            .nodeSize([nodeWidth + horizontalSeparationBetweenNodes, nodeHeight + verticalSeparationBetweenNodes])
+            .separation(function(a, b) {
+                return a.parent == b.parent ? 1 : 1.25;
+        });
 
         var diagonal = d3.svg.diagonal()
-          .projection(function(d) { return [d.y, d.x]; });
+          .projection(function(d) { return [d.x, d.y]; });
 
         var svg = d3.select("#stock_d3").append("svg")
           .attr("width", width + margin.right + margin.left)
@@ -99,7 +121,9 @@ odoo.define('stock_d3.index', function (require) {
             links = tree.links(nodes);
 
           // Normalize for fixed-depth.
-          nodes.forEach(function(d) { d.y = d.depth * 180; });
+          nodes.forEach(function(d) {
+            d.y = d.depth * 90;
+          });
 
           // Update the nodes…
           var node = svg.selectAll("g.node")
@@ -108,42 +132,59 @@ odoo.define('stock_d3.index', function (require) {
           // Enter any new nodes at the parent's previous position.
           var nodeEnter = node.enter().append("g")
             .attr("class", "node")
-            .attr("transform", function(d) { return "translate(" + source.y0 + "," + source.x0 + ")"; })
+            .attr("transform", function(d) { return "translate(" + source.x0 + "," + source.y0 + ")"; })
             .on("click", click);
 
-          nodeEnter.append("circle")
-            .attr("r", 1e-6)
+          nodeEnter.append("rect")
+            .attr("width", 5)
+            .attr("height", 5)
+            .attr("y", 0)
+            .attr("x", 0)
             .style("fill", function(d) { return d._children ? "lightsteelblue" : "#fff"; });
 
           nodeEnter.append("text")
-            .attr("x", function(d) { return d.children || d._children ? -13 : 13; })
-            .attr("dy", ".35em")
-            .attr("text-anchor", function(d) { return d.children || d._children ? "end" : "start"; })
-            .text(function(d) { return d.name; })
-            .style("fill-opacity", 1e-6);
+            .append("tspan")
+        	  .attr("x", function(d) { return d.children || d._children ? 0 : 0; })
+        	  .attr("dy", "-0.2em")
+        	  .attr("text-anchor", function(d) { return d.children || d._children ? "end" : "start"; })
+        	  .text(function(d) { return d.name; })
+            .append("tspan")
+        	  .attr("x", function(d) { return d.children || d._children ? 0 : 0; })
+        	  .attr("dy", "1.1em")
+        	  .attr("text-anchor", function(d) { return d.children || d._children ? "end" : "start"; })
+        	  .text(function(d) { return d.product; });
 
           // Transition nodes to their new position.
           var nodeUpdate = node.transition()
             .duration(duration)
-            .attr("transform", function(d) { return "translate(" + d.y + "," + d.x + ")"; });
+            .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
 
-          nodeUpdate.select("circle")
-            .attr("r", 10)
+          nodeUpdate.select("rect")
+            .attr("width", 120)
+            .attr("height", 50)
+            .attr("y", -25)
+            .attr("x", -60)
+            //.attr("x", function(d) { return d.children || d._children ? -80 : 0; })
             .style("fill", function(d) { return d._children ? "lightsteelblue" : "#fff"; });
 
-          nodeUpdate.select("text")
+          nodeUpdate.selectAll("tspan")
+            .attr("x", function(d) { return d.children || d._children ? 0 : 0; })
+            .style("text-anchor", "middle")
             .style("fill-opacity", 1);
 
           // Transition exiting nodes to the parent's new position.
           var nodeExit = node.exit().transition()
             .duration(duration)
-            .attr("transform", function(d) { return "translate(" + source.y + "," + source.x + ")"; })
+            .attr("transform", function(d) { return "translate(" + source.x + "," + source.y + ")"; })
             .remove();
 
-          nodeExit.select("circle")
-            .attr("r", 1e-6);
+          nodeExit.select("rect")
+            .attr("y", 0)
+            .attr("x", 0)
+            .attr("width", 5)
+            .attr("height", 5);
 
-          nodeExit.select("text")
+          nodeExit.selectAll("tspan")
             .style("fill-opacity", 1e-6);
 
           // Update the links…
@@ -169,13 +210,12 @@ odoo.define('stock_d3.index', function (require) {
             .attr("d", function(d) {
             var o = {x: source.x, y: source.y};
             return diagonal({source: o, target: o});
-            })
-            .remove();
+            }).remove();
 
           // Stash the old positions for transition.
           nodes.forEach(function(d) {
-          d.x0 = d.x;
-          d.y0 = d.y;
+            d.x0 = d.x;
+            d.y0 = d.y;
           });
         }
 
